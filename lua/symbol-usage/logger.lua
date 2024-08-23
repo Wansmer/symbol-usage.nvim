265eb0d2496c84c4c2187a7154a733a019ca9f7d
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-08-23 14:06:28 +0300

    refactor (logger): made it clearer

--- lua/symbol-usage/logger.lua
+++ lua/symbol-usage/logger.lua
@@ -11,66 +11,68 @@ local levels = vim.deepcopy(vim.log.levels)
 ---@field log_file { enabled: boolean, path: string }
 ---@field notify { enabled: boolean }
 local defaut_config = {
-  enabled = true,
-  level = 'DEBUG',
-  stdout = {
-    enabled = true,
-    hl = { TRACE = 'None', DEBUG = 'Debug', INFO = 'DiagnosticHint', WARN = 'WarningMsg', ERROR = 'ErrorMsg' },
-  }, -- should log to stdout or not
+  enabled = false,
+  level = 'INFO',
   log_file = {
     enabled = true,
     path = vim.fs.joinpath(vim.fn.stdpath('cache'), 'symbol-usage.log'),
   },
+  stdout = {
+    enabled = false,
+    hl = { TRACE = 'None', DEBUG = 'Debug', INFO = 'DiagnosticHint', WARN = 'WarningMsg', ERROR = 'ErrorMsg' },
+  },
   notify = { enabled = false }, -- should be use `vim.notify` or not
 }
 
+---@class Logger
+---@field config LoggerConfig
+---@field [LogLevelLowercase] function(...):void
+local Logger = {}
+Logger.__index = Logger
+
+---Create new logger
+---@param config LoggerConfig?
+---@return Logger
+function Logger.new(config)
+  local logger = setmetatable({
+    config = vim.tbl_deep_extend('force', defaut_config, config or {}),
+  }, Logger)
+
+  for level, _ in pairs(levels) do
+    Logger[level:lower()] = function(...)
+      Logger.log(logger, level, ...)
+    end
+  end
+
+  return logger
+end
+
 ---Log message according to level
----@param Logger Logger
 ---@param level LogLevel
 ---@vararg any[]
-local function logger(Logger, level, ...)
-  if not Logger.config.enabled or levels[level] < levels[Logger.config.level] then
+function Logger:log(level, ...)
+  if not self.config.enabled or levels[level] < levels[self.config.level] then
     return
   end
 
   local timestamp = os.date('%Y-%m-%d %H:%M:%S')
   local msg = string.format('%s [%s] %s', timestamp, level, vim.inspect(...))
 
-  if Logger.config.log_file and Logger.config.log_file.path ~= '' then
-    vim.fn.writefile({ timestamp .. ' ' .. msg }, Logger.config.log_file.path, 'a')
+  if self.config.log_file and self.config.log_file.path ~= '' then
+    vim.fn.writefile({ timestamp .. ' ' .. msg }, self.config.log_file.path, 'a')
   end
 
-  if Logger.config.stdout then
-    vim.api.nvim_echo({ { msg, Logger.config.stdout.hl[level] or 'None' } }, true, {})
+  if self.config.stdout then
+    vim.api.nvim_echo({ { msg, self.config.stdout.hl[level] or 'None' } }, true, {})
   end
 
-  if Logger.config.notify then
+  if self.config.notify.enabled then
     vim.notify(msg, levels[level])
   end
 end
 
----@class Logger
----@field config LoggerConfig
----@field [LogLevelLowercase] function(...):void
-local M = {}
-M.__index = function(self, index)
-  local maybe_level = string.upper(index)
-  if levels[maybe_level] then
-    return function(...)
-      return logger(self, maybe_level, ...)
-    end
-  else
-    return self[index]
-  end
+function Logger:update_config(new_config)
+  self.config = vim.tbl_deep_extend('force', self.config, new_config)
 end
 
----Create new logger
----@param config LoggerConfig?
----@return Logger
-function M.new(config)
-  return setmetatable({
-    config = vim.tbl_deep_extend('force', defaut_config, config or {}),
-  }, M)
-end
-
-return M
+return Logger
